59cee7856dd60a5dc91a8412e44c55d51b518ffe
Bacconnier Agathe <bacconni@insa-20730>
2021-04-07 15:04:05 +0200

    implémentation du programme from scratch dans launch.java

--- be-graphes-gui/src/main/java/org/insa/graphs/gui/simple/Launch.java
+++ be-graphes-gui/src/main/java/org/insa/graphs/gui/simple/Launch.java
@@ -54,20 +54,23 @@ public class Launch {
                 new DataInputStream(new BufferedInputStream(new FileInputStream(mapName))));
 
         // TODO: Read the graph.
-        final Graph graph = null;
+        final Graph graph = reader.read();
 
         // Create the drawing:
         final Drawing drawing = createDrawing();
 
         // TODO: Draw the graph on the drawing.
+        drawing.drawGraph(graph);
 
         // TODO: Create a PathReader.
-        final PathReader pathReader = null;
+        final PathReader pathReader =  new BinaryPathReader(
+        		new DataInputStream(new BufferedInputStream(new FileInputStream(pathName))));
 
         // TODO: Read the path.
-        final Path path = null;
+        final Path path = pathReader.readPath(graph);
 
         // TODO: Draw the path.
+        drawing.drawPath(path);
     }
 
 }
